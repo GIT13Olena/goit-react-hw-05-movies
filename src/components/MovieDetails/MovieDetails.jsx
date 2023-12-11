488e3bba1ab2e67ebe6f9740972ea87c4cccc848
GIT13Olena <girnyak.1olena@gmail.com>
2023-12-12 00:33:53 +0200

    changed styles, added a mod for actors

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieDetails, getMovieCast, getMovieReviews } from '../api';
+
+import star from '../../icons/star.svg';
+
 import './movieDetalis.module.css';
 
 function MovieDetails() {
@@ -8,8 +11,8 @@ function MovieDetails() {
   const [movie, setMovie] = useState(null);
   const [cast, setCast] = useState([]);
   const [reviews, setReviews] = useState([]);
-  const [isCastExpanded, setIsCastExpanded] = useState(false);
   const [isReviewsExpanded, setIsReviewsExpanded] = useState(false);
+  const [isModalMoviesOpen, setIsModalMoviesOpen] = useState(false);
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -44,9 +47,9 @@ function MovieDetails() {
     fetchMovieReviews();
   }, [movieId]);
 
-  const toggleCast = () => {
-    setIsCastExpanded(!isCastExpanded);
-  };
+  // const toggleCast = () => {
+  //   setIsCastExpanded(!isCastExpanded);
+  // };
 
   const toggleReviews = () => {
     setIsReviewsExpanded(!isReviewsExpanded);
@@ -57,6 +60,28 @@ function MovieDetails() {
   }
 
   const { title, overview, poster_path, genres, vote_average } = movie;
+
+  const openModalMovies = () => {
+    setIsModalMoviesOpen(true);
+  };
+
+  const closeModalMovies = () => {
+    setIsModalMoviesOpen(false);
+  };
+
+  const handleOnClose = e => {
+    if (e.code === 'Escape') {
+      closeModalMovies();
+    }
+  };
+
+  window.addEventListener('keydown', handleOnClose);
+
+  const handleOverlyClick = e => {
+    if (e.currentTarget === e.target) {
+      closeModalMovies();
+    }
+  };
 
   return (
     <div className="details-container">
@@ -69,53 +94,69 @@ function MovieDetails() {
 
         <div className="details-right">
           <div className="div-detalis-film">
-            <h1>{title}</h1>
-            <h2>Overview</h2>
+            <div className="title-evaluation">
+              <h1>{title}</h1>
+              <div className="star-container">
+                <img src={star} alt="svg star" className="svg-star" />
+                <p className="evaluation">{vote_average.toFixed(1)}</p>
+              </div>
+            </div>
+
+            <h2 className="title-overview-genres">Overview</h2>
             <p>{overview}</p>
-            <h2>Genres</h2>
+            <h2 className="title-overview-genres">Genres</h2>
             <p> {genres.map(genre => genre.name).join(', ')}</p>
-            <p>User Score: {vote_average}</p>
           </div>
         </div>
       </div>
 
-      <div className="div-additional-info">
-        <h2 className="h2-additional-info">Additional information</h2>
-        <div>
-          <button onClick={toggleCast} className="button-cast">
-            {isCastExpanded ? 'Cast' : 'Cast'}
-          </button>
-          {isCastExpanded && (
-            <ul className="ul-actors">
-              {cast.map(member => (
-                <li key={member.id}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w200/${member.profile_path}`}
-                    alt={member.name}
-                  />
-                  <p className="ul-actors-p">
-                    {member.name}
-                    <br />
-                    Character: {member.character}
-                  </p>
-                </li>
-              ))}
-            </ul>
-          )}
-          <button onClick={toggleReviews} className="button-reviews">
-            {isReviewsExpanded ? 'Reviews' : 'Reviews'}
-          </button>
-          {isReviewsExpanded && (
-            <ul className="ul-all-reviews">
-              {reviews.map(review => (
-                <li key={review.id}>
-                  <h2>Author: {review.author}</h2>
-                  <p>{review.content}</p>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+      <h2 className="h2-additional-info">Additional information</h2>
+
+      <div>
+        <button onClick={openModalMovies} className="button-cast">
+          Cast
+        </button>
+
+        {isModalMoviesOpen && (
+          <div className="modalOverly" onClick={handleOverlyClick}>
+            <div className="modalContent">
+              <button onClick={closeModalMovies} className="closeModalBtn">
+                &#10006;
+              </button>
+
+              <ul className="ul-actors">
+                {cast.map(member => (
+                  <li key={member.id}>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w200/${member.profile_path}`}
+                      alt={member.name}
+                    />
+                    <p className="ul-actors-p">
+                      {member.name}
+                      <br />
+                      Character: {member.character}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+
+        <button onClick={toggleReviews} className="button-reviews">
+          Reviews
+        </button>
+
+        {isReviewsExpanded && (
+          <ul className="ul-all-reviews">
+            {reviews.map(review => (
+              <li key={review.id}>
+                <h2>Author: {review.author}</h2>
+                <p>{review.content}</p>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
